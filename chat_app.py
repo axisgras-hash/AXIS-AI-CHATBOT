b157469be7278b95937d9105565d2196b9999ac4
axisgras-hash <axisgras@gmail.com>
2025-12-29 15:52:50 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -67,10 +67,11 @@ if user_prompt:
 if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
-    st.experimental_rerun()
+    st.rerun()
 
 
 st.markdown('**Design And Developed by: Ankit Mishra**')
+
 
 
 
